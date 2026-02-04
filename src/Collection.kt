974209206584459108350e375c89a4cddd47960f
dragonyafa134 <dasha.afanaseva.1206@mail.ru>
2026-02-04 10:16:32 +0300

    Lists in Kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -28,5 +28,35 @@ fun main(){
 //
 //    println(solarSystem[3])
 
-    println(newSolarSystem[8])
+//    println(newSolarSystem[8])
+
+//    val solarSystem = listOf(
+//        "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+//    println(solarSystem.size)
+//
+//    println(solarSystem[2])
+//    println(solarSystem.get(3))
+//
+//    println(solarSystem.indexOf("Earth"))
+//    println(solarSystem.indexOf("Pluto"))
+//
+//    for (planet in solarSystem){
+//        println(planet)
+//    }
+//
+//    mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+//
+//    solarSystem.add("Pluto")
+//    solarSystem.add(index=3, element = "Theia")
+
+//    solarSystem[3] = "Future Moon"
+//    println(solarSystem[3])
+//    println(solarSystem[9])
+//
+//    solarSystem.removeAt(index=9)
+//
+//    println(solarSystem.contains("Pluto"))
+//    println("Future Moon" in solarSystem)
+
+
 }
