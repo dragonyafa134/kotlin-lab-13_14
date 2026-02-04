9fea67e48b0984dcc2148676e0b357e360f6787a
dragonyafa134 <dasha.afanaseva.1206@mail.ru>
2026-02-04 09:21:45 +0300

    Use a singleton object

--- src/Question.kt
+++ src/Question.kt
@@ -9,14 +9,20 @@ data class Question<T>(
     val anserw : T,
     val difficulrt : Difficulty
 )
-
-
-
-fun main(){
+class Quiz{
     val question1 = Question(questionText =  "Речка спрятана"  , "Водопровод" ,Difficulty.MEDIUM)
     val question2 = Question(questionText =  "Небо зеленная"  , false ,Difficulty.EASY)
     val question3 = Question(questionText =  "Сколько дней между полнолуниями"  , "28" ,Difficulty.HARD)
-    println(question1.anserw)
-    println(question2.anserw)
-    println(question3.anserw)
+
+    companion object StudentProgress {
+        var total : Int = 10
+        var answered : Int = 3
+    }
+}
+
+
+fun main(){
+
+    //println("${StudentProgress.answered} of ${StudentProgress.total} anserwed")
+    println("${Quiz.answered} of ${Quiz.total} anserwed")
 }
