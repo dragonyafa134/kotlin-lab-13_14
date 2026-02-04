271da78b9c4b0ed3b89b13122e8368b20adf60c6
dragonyafa134 <dasha.afanaseva.1206@mail.ru>
2026-02-04 13:19:02 +0300

    sortedBy() function

--- src/HigherOrderFunctionsList.kt
+++ src/HigherOrderFunctionsList.kt
@@ -84,10 +84,18 @@ fun main(){
 //    }
 
 
-    val totalPrice = cookies.fold(0.0){
-        total, cookie ->
-        total + cookie.price
+//    val totalPrice = cookies.fold(0.0){
+//        total, cookie ->
+//        total + cookie.price
+//    }
+//    println("Total price : ${totalPrice}")
+
+
+
+    val alphabeticalMenu = cookies.sortedBy{it.name}
+    println("Меню в алфавитном порядке")
+    alphabeticalMenu.forEach {
+        println(it.name)
     }
-    println("Total price : ${totalPrice}")
 
 }
