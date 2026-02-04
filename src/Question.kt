a28d9cf1df47102ba489b9294c7f1a4ab44ada01
dragonyafa134 <dasha.afanaseva.1206@mail.ru>
2026-02-04 09:44:20 +0300

    Rewrite extensions using interface

--- src/Question.kt
+++ src/Question.kt
@@ -9,7 +9,21 @@ data class Question<T>(
     val anserw : T,
     val difficulrt : Difficulty
 )
-class Quiz{
+
+interface ProgressPrintable{
+    val progressText : String
+}
+class Quiz : ProgressPrintable {
+    override val progressText: String
+        get() = "${answered} of ${total} answerd"
+
+    fun printProgressBar(){
+        repeat(Quiz.answered) {println("▓")}
+        repeat(Quiz.total - Quiz.answered) {println("▒")}
+        println()
+        println(progressText)
+    }
+
     val question1 = Question(questionText =  "Речка спрятана"  , "Водопровод" ,Difficulty.MEDIUM)
     val question2 = Question(questionText =  "Небо зеленная"  , false ,Difficulty.EASY)
     val question3 = Question(questionText =  "Сколько дней между полнолуниями"  , "28" ,Difficulty.HARD)
@@ -20,20 +34,22 @@ class Quiz{
     }
 }
 
-val Quiz.StudentProgress.progressText : String
-    get() = "${answered} of ${total} anserwd"
+//val Quiz.StudentProgress.progressText : String
+//    get() = "${answered} of ${total} anserwd"
 
-fun Quiz.StudentProgress.printProgressBar(){
-    repeat(Quiz.answered) {println("▓")}
-    repeat(Quiz.total - Quiz.answered) {println("▒")}
-    println()
-    println(Quiz.progressText)
-}
+//fun Quiz.StudentProgress.printProgressBar(){
+//    repeat(Quiz.answered) {println("▓")}
+//    repeat(Quiz.total - Quiz.answered) {println("▒")}
+//    println()
+//    println(Quiz.progressText)
+
+//}
 
 fun main(){
 
     //println("${StudentProgress.answered} of ${StudentProgress.total} anserwed")
 //    println("${Quiz.answered} of ${Quiz.total} anserwed")
 //    println(Quiz.progressText)
-    Quiz.printProgressBar()
+//    Quiz.printProgressBar()
+    Quiz().printProgressBar()
 }
