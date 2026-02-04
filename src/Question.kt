fe8621b185455f782ef1e36419c55cccc8b81cf0
dragonyafa134 <dasha.afanaseva.1206@mail.ru>
2026-02-04 09:09:49 +0300

    Use an enum class

--- src/Question.kt
+++ src/Question.kt
@@ -1,13 +1,21 @@
+enum class Difficulty{
+    EASY,
+    MEDIUM,
+    HARD
+}
+
 class Question<T>(
     val questionText : String,
     val anserw : T,
-    val difficulrt : String
+    val difficulrt : Difficulty
 )
 
+
+
 fun main(){
-    val question1 = Question(questionText =  "Речка спрятана"  , "Водопровод" ,"medium")
-    val question2 = Question(questionText =  "Небо зеленная"  , false ,"easy")
-    val question3 = Question(questionText =  "Сколько дней между полнолуниями"  , "28" ,"hard")
+    val question1 = Question(questionText =  "Речка спрятана"  , "Водопровод" ,Difficulty.MEDIUM)
+    val question2 = Question(questionText =  "Небо зеленная"  , false ,Difficulty.EASY)
+    val question3 = Question(questionText =  "Сколько дней между полнолуниями"  , "28" ,Difficulty.HARD)
     println(question1.anserw)
     println(question2.anserw)
     println(question3.anserw)
