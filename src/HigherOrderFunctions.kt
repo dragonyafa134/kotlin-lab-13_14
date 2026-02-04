0eb9bb12264e19babdb77843e4434cead0f76a8a
dragonyafa134 <dasha.afanaseva.1206@mail.ru>
2026-02-04 10:41:58 +0300

    Higher order functions

---
+++ src/HigherOrderFunctions.kt
@@ -0,0 +1,64 @@
+//fun main(){
+//    displayMessage(mes= ::morning)
+//    displayMessage(mes= ::evening)
+//}
+//fun displayMessage(mes : () -> Unit){
+//    mes()
+//}
+//fun morning(){
+//    println("Good moring")
+//}
+//fun evening(){
+//    println("Good Evening")
+//}
+
+//
+//fun main(){
+//    action(n1 = 5, n2 = 3 , op=::sum)
+//    action(5,3, ::multiply)
+//    action(5,3,::subtract)
+//}
+//
+//fun action(n1:Int, n2:Int, op:(Int, Int) -> Int){
+//    val result = op(n1,n2)
+//    println(result)
+//}
+//fun sum (a:Int, b:Int): Int {
+//    return a + b
+//}
+//fun subtract(a: Int, b: Int) : Int{
+//    return a - b
+//}
+//fun multiply(a: Int, b: Int): Int{
+//    return a * b
+//}
+
+
+fun selectAction(key : Int ): (Int , Int) -> Int{
+    return when(key){
+        1 -> ::sum
+        2 -> ::subtract
+        3 -> ::multiply
+        else -> ::empty
+    }
+}
+
+fun empty(a:Int, b:Int): Int{
+    return 0
+}
+fun sum (a:Int, b:Int): Int {
+    return a + b
+}
+fun subtract(a: Int, b: Int) : Int{
+    return a - b
+}
+fun multiply(a: Int, b: Int): Int {
+    return a * b
+}
+
+fun main(){
+    val action1 = selectAction(1)
+    println(action1(8,5))
+    val action2 = selectAction(2)
+    println(action2(8,5))
+}
